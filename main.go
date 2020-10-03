5f46dd25162a8ea02bb6140e7749b8a305ae04d8
Justin Zollars <142996+fogonthedowns@users.noreply.github.com>
2020-10-02 21:17:28 -0700

    changes

--- main.go
+++ main.go
@@ -4,33 +4,40 @@ import (
 	"sort"
 )
 
+// left and right
+// X-axis of building
+const LEFTSIDE = 1
+const RIGHTSIDE = 2
+
 func getSkyline(buildings [][]int) [][]int {
 	var ret [][]int
 	if len(buildings) == 0 {
 		return ret
 	}
 
-	points := make([][]int, 0)
+	points := make([]Point, 0)
 
+	// [x-axis (value), [1 (left) | 2 (right)], index (building number)]
 	for i := 0; i < len(buildings); i++ {
 		b := buildings[i]
-		p1 := []int{b[0], 1, i}
-		p2 := []int{b[1], 2, i}
-		points = append(points, p1)
-		points = append(points, p2)
+		left := Point{xAxis: b[0], side: LEFTSIDE, index: i}
+		right := Point{xAxis: b[1], side: RIGHTSIDE, index: i}
+		points = append(points, left)
+		points = append(points, right)
 	}
 
 	// first sort inner
-	sort.SliceStable(points, func(i, j int) bool { return points[i][1] < points[j][1] })
+	sort.SliceStable(points, func(i, j int) bool { return points[i].side < points[j].side })
 	// next sort first
-	sort.SliceStable(points, func(i, j int) bool { return points[i][0] < points[j][0] })
+	sort.SliceStable(points, func(i, j int) bool { return points[i].xAxis < points[j].xAxis })
 
 	bit := make(BIT, len(points)+1)
 
+	// maps all points on the x-axis
 	mp := make(map[int]int)
 	for i := 0; i < len(points); i++ {
-		if mp[points[i][0]] == 0 {
-			mp[points[i][0]] = i
+		if mp[points[i].xAxis] == 0 {
+			mp[points[i].xAxis] = i
 		}
 	}
 
@@ -40,15 +47,16 @@ func getSkyline(buildings [][]int) [][]int {
 		var L int
 		var R int
 		var H int
-		if pt[1] == 1 {
+
+		if pt.side == LEFTSIDE {
 			// start of a building
-			building := buildings[pt[2]]
+			building := buildings[pt.index]
 			L = building[0]
 			R = building[1]
 			H = building[2]
 			add(bit, mp[R], H)
 		} else {
-			L = buildings[pt[2]][1]
+			L = buildings[pt.index][1]
 		}
 
 		cur := find(bit, mp[L]+1)
@@ -71,6 +79,11 @@ func getSkyline(buildings [][]int) [][]int {
 }
 
 type BIT []int
+type Point struct {
+	xAxis int
+	side  int
+	index int
+}
 
 // builds the BIT, i increases with the opp i -= i & (-i)
 // uses max(cur, height)
